10da81f5177082d46a83dc2c1de7f38b19490c30
DenysRogeres <dennysrogeres@gmail.com>
2021-11-10 23:19:48 -0300

    Adding Page1

---
+++ src/components/Page1.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="teste ">
+        <img src="../assets/squareFrame.png" alt="Square image">
+    </div>
+</template>
+
+<style>
+    img {
+        height: 100%;
+    }
+</style>
