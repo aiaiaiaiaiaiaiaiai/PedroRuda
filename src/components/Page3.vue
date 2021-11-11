fe4f17836d539c3bdf3594ef2a55e2d2c0f3903c
DenysRogeres <dennysrogeres@gmail.com>
2021-11-10 23:20:40 -0300

    Adding page 3

---
+++ src/components/Page3.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <div><img style="width: 200px" src="../assets/filme_pg-3.gif" alt=""></div>
+        <div><img style="width: 200px" src="../assets/picture_pg-3.png" alt=""></div>
+    </div>
+</template>
