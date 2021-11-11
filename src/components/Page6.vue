620a81ac4bc02aac772a5a4ce96bd0c56e770120
DenysRogeres <dennysrogeres@gmail.com>
2021-11-10 23:21:46 -0300

    Adding page 6

---
+++ src/components/Page6.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div><img style="width: 200px" src="../assets/filme1_pg-6.gif" alt=""></div>
+        <div><img style="width: 200px" src="../assets/filme2_pg-6.png" alt=""></div>
+        <div><img style="width: 200px" src="../assets/filme3_pg-6.gif" alt=""></div>
+    </div>
+</template>
