2d3a228031af05479b824a0fc37630717f4b7216
DenysRogeres <dennysrogeres@gmail.com>
2021-11-10 23:20:17 -0300

    Adding Page 2

---
+++ src/components/Page2.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="teste1">
+        <div><img style="width: 200px" src="../assets/circleFrame.png" alt="Circle Frame"></div>
+        <div><img style="width: 200px;" src="../assets/filme1_pg-2.gif" alt="Filme 1"></div>
+        <div><img style="width: 200px;" src="../assets/filme2_pg-2.gif" alt="Filme 2"> </div>
+        <div><img style="width: 200px;" src="../assets/filme3_pg-2.gif" alt="Filme 3"></div>
+        <div><img style="width: 200px;" src="../assets/filme4_pg-2.gif" alt="Filme 4"> </div>
+    </div>
+</template>
+
+<style>
+    .teste1 {
+       background: black;
+    }
+</style>
