b79a84134064884f77e5b5685ff8575b698e87c0
aiaiaiaiaiaiaiaiai <87497751+aiaiaiaiaiaiaiaiai@users.noreply.github.com>
2021-11-13 14:12:34 -0300

    removendo package lock
    removendo package lock

--- src/components/Page1.vue
+++ src/components/Page1.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="teste ">
-        <img style="height: 100vh" src="../assets/squareFrame.png" alt="Square image">
+        <img style="width: 100vw" src="../assets/squareFrame.png" alt="Square image">
     </div>
 </template>
