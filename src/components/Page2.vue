a2662f77eb5e9e7e81976e905e9e94e13c066bc5
aiaiaiaiaiaiaiaiai <87497751+aiaiaiaiaiaiaiaiai@users.noreply.github.com>
2021-11-15 11:07:00 -0300

    correcao pag 2 nao carregava

--- src/components/Page2.vue
+++ src/components/Page2.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="fundo2">
+    <div class="pag2">
         <div><img style="width: 60%; margin-bottom: 400px;" src="../assets/circleFrame.png" alt="Circle Frame"></div>
         <div><img style="width: 100%; margin-bottom: 400px;" src="../assets/filme1_pg-2.gif" alt="Filme 1"></div>
         <div><img style="width: 100%; margin-bottom: 200px;" src="../assets/filme2_pg-2.gif" alt="Filme 2"> </div>
@@ -9,7 +9,7 @@
 </template>
 
 <style>
-.fundo2{    
+.pag2{    
     background: pink;
 }
 </style>
